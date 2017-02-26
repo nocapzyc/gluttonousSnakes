8653857adfef3f8dd12f427a35ab64e403f11968
SheliaWu <shelia.wu4575@gmail.com>
2017-02-26 15:29:50 +0800

    功能全部完成，正常运行

--- snake.js
+++ snake.js
@@ -1,20 +1,19 @@
 (function($){
 
-
+   /*默认配置*/
 	var defaults={
 		 width: $('.stage').width(),
 		 height: $('.stage').height(),
          cellWidth: 20,
-		 speed: 1,
+		 speed: 2,
 		 initialLength: 3,
-         snakeLength:3,
          eatNum:$('#num'),
          score:$('#score'),
          recordTable:$('.record table')
 
 	}
 
-
+    /*创建对象*/
 	var Snake=function(options){
 		this.options=$.extend({},defaults,options);
         this.width=this.options.width;
@@ -22,13 +21,13 @@
         this.cellWidth=this.options.cellWidth;
         this.speed=this.options.speed;
         this.initialLength=this.options.initialLength;
-        this.snakeLength=this.options.snakeLength;
+        this.snakeLength=this.options.initialLength;
         this.$stage=this.options.stage;
         this.$eatNum=this.options.eatNum;
         this.$score=this.options.score;
         this.$recordTable=this.options.recordTable;
 
-        this.record=new Array();
+        this.record='';
         this.grid=new Array();
         this.snake=new Array();
         this.food=new Array();
@@ -41,6 +40,7 @@
 
 	}
 
+    /*原型方法封装*/
     Snake.prototype={
 
         init:function(){
@@ -48,6 +48,7 @@
             
         },
 
+        /*开始游戏函数*/
         initPlay:function(){
             if(this.die){
 
@@ -57,26 +58,29 @@
                     this.grid[x][y].removeClass('snake');
                 }
 
-                console.log('destroy'+this.food);
                 var x=this.food[0][0],
                     y=this.food[0][1];
                 this.grid[x][y].removeClass('food');
 
                 this.snakeLength=this.initialLength;
+                this.direction=1;
                 this.num=0;
                 this.score=0;
                 this.$eatNum.text(this.num);
                 this.$score.text(this.score);
             }
+
             this.createSnake();
             this.createFood();
             $("#start").attr("disabled",true);
             this.setTimer();
 
         },
-
+        /*开始渲染游戏地图和排行榜*/
         drawStage:function(){
-            console.log('drawStage');
+            //render排行榜
+            this.renderTable();
+
             var $table=$('.stage');
             for(var i=0;i<this.height/this.cellWidth;i++){
                 this.grid[i]=new Array();
@@ -90,17 +94,17 @@
             }
         },
 
+        /*地图生成小蛇*/
         createSnake:function(){
             this.snake=this.randomObject(this.options.initialLength,'snake');
         },
-
+        /*地图生成食物*/
         createFood:function(){
             this.food=this.randomObject(1,'food');
-            console.log('create'+this.food);
         },
 
 
-        //生成食物或者小蛇
+        //生成食物或者小蛇函数
         randomObject:function(len,name){
             var obj=[],
             x=Math.floor(Math.random()*this.height/this.cellWidth),
@@ -127,6 +131,7 @@
             return obj;
         },
 
+        /*keydown监听函数*/
         changeDirection: function(event){
                 var e = event || window.event || arguments.callee.caller.arguments[0];
                 var direction=this.direction;
@@ -157,14 +162,16 @@
                     break;
                 }
         },
+        /*绑定事件*/
         bindEvent:function(){
             //里面的this指定了特定的上下文，是snake原型而不是触发事件的动漫元素
             //不能用this.init(),这是立即执行函数
             $('#playInit').on('click',$.proxy(this.drawStage,this));
             $(document).on('keydown',$.proxy(this.changeDirection,this));
             $('#start').on('click',$.proxy(this.initPlay,this));
+            $('#save').on('click',$.proxy(this.updateRecord,this));
         },
-
+        /*小蛇移动函数*/
         snakeMove:function(){
             this.direction=this.nextDirection||this.direction;
             this.nextDirection='';
@@ -181,7 +188,7 @@
              if(headX>=this.height/this.cellWidth||headX<0||headY>=this.width/this.cellWidth||headY<0||this.grid[headX][headY].hasClass('snake')){
                 this.die=true;
                 //模态框
-
+                $('#myModal').modal('show');
                 $('#start').removeAttr('disabled');
                 clearTimeout(this.snakeTimer);
                 return;
@@ -209,13 +216,57 @@
 
              this.setTimer();
         },
-
+        /*定时使小蛇移动*/
         setTimer:function(){
-            this.snakeTimer=setTimeout($.proxy(this.snakeMove,this),500);
+            this.snakeTimer=setTimeout($.proxy(this.snakeMove,this),800/this.speed);
+        },
+        /*更新排行榜*/
+        updateRecord:function(){
+            var username=$('#username').val();
+            $('#username').val('');
+            $('#myModal').modal('hide');
+            var score=this.score;
+            var newRecord=[username,score];
+            this.renderTable(newRecord);           
+        },
+        /*渲染排行榜*/
+        renderTable:function(update){
+            var storage=window.localStorage;
+            if(!storage.hasOwnProperty('record')){
+                var arr=[];
+                storage.setItem('record',JSON.stringify(arr));
+            }
+            var str=storage.getItem('record');
+            this.record=JSON.parse(str);
+            if(update){
+                if(this.record.length<5){
+                    this.record.push(update);
+                }else{
+                 if(update[1]>this.record[this.record.length-1][1]){
+                    this.record.pop();
+                    this.record.push(update);
+                    }
+                }
+            }
+
+            this.record.sort(function(a,b){
+                return b[1]-a[1];
+            })
+
+            str=JSON.stringify(this.record);
+            storage.setItem('record',str);
+            //console.log(storage.record+' storage');
+
+            for(var i=0;i<this.record.length;i++){
+                var tr=this.$recordTable.find('tr').eq(i+1);
+                tr.find('td').eq(1).text(this.record[i][0]);
+                tr.find('td').eq(2).text(this.record[i][1]);
+            }
+
         }
     }
 
-
+//执行
  
 var snake=new Snake({});
 snake.init();
